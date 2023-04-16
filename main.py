3e80ce4cbafa365df8333b6fe097cbe38a6113ad
Chris Micallef <chrismicallef05@gmail.com>
2023-04-16 23:02:18 +0100

    Added utils-quantitive. Refactored significantly

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-from graphing.simple_ma_graphs import plot_strat_df
-from strategies.simple_ma import build_ma_strat_df, build_returns_df
+
 
 
 def main():
     """
     Main func to faff in
     """
-    tkr = 'AMZN'
-    strat_df = build_ma_strat_df(tkr)
-    plot_strat_df(strat_df, tkr)
-    returns_df = build_returns_df(tkr, strat_df)
-    returns_df.to_csv(f'out/{tkr}_strat_details.csv')
+    
 
 
 if __name__ == '__main__':
---
+++ strategies/.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from data_scraping.twelve_data.twelve_data_scrape import get_csv_data
+import numpy as np
+import pprint as pprint
+import matplotlib.pyplot as plt
+from datetime import timedelta
+
+
+
+
+
