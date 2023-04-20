94565a04c393248442b04fef7382fc5ee9843944
Chris Micallef <chrismicallef05@gmail.com>
2023-04-20 17:41:30 +0100

    Began converting to sql db

--- data_scraping/twelve_data/twelve_data_scrape.py
+++ data_scraping/twelve_data/twelve_data_scrape.py
@@ -27,27 +27,9 @@ output:
     csv files in data_scraping/twelve_data for each ticker
 '''
 
-def ticker_to_exchange_map(ticker, exchange_name):
-    hashmap_val = {ticker : exchange_name}
-    if exists(fr'{data_dir}/exchange_mapping.pickle'):
-        with open(f'{data_dir}/exchange_mapping.pickle', 'rb') as f:
-            hashmap = pickle.load(f)
-            hashmap[ticker] = exchange_name
-        with open(f'{data_dir}/exchange_mapping.pickle', 'wb') as f:
-            pickle.dump(hashmap, f)
-    else:
-        with open(f'{data_dir}/exchange_mapping.pickle', 'wb') as f:
-            pickle.dump(hashmap_val, f)
-
-def exchange_name_check(ticker):
-    with open(f'{data_dir}/exchange_mapping.pickle', 'rb') as f:
-        hashmap = pickle.load(f)
-        exchange_name = hashmap[ticker] 
-    return exchange_name
-
 def get_api_data(ticker, days):
-    if exists(fr'{data_dir}/{ticker}.csv') and exists(fr'{data_dir}/{ticker}.json'):
-            return
+    if file_exists_in_folder(data_dir, f'{ticker}.csv'):
+        return
     tickers_with_err_msg = []
     conn = http.client.HTTPSConnection("twelve-data1.p.rapidapi.com")
 
@@ -63,14 +45,12 @@ def get_api_data(ticker, days):
     try:
         price_data_df = pd.DataFrame(price_json['values'])
         meta_data_json = price_json['meta']
+        append_to_meta_data(ticker, meta_data_json)
         exchange_name = meta_data_json['exchange']
-        ticker_to_exchange_map(ticker, exchange_name)
         subfolder = os.path.join(data_dir, exchange_name)
         if not os.path.exists(subfolder):
             os.makedirs(subfolder)
 
-        with open(os.path.join(subfolder, f"{ticker}.json"), "w") as outfile:
-            json.dump(meta_data_json, outfile)
         price_data_df.to_csv(os.path.join(subfolder, f"{ticker}.csv"))
 
     except KeyError:
@@ -92,12 +72,39 @@ def get_stock_data(ticker):
     return df
 
 
+def append_to_meta_data(ticker, meta_data):
+    meta_file_path = os.path.join(data_dir, "meta_data.json")
+    
+    if not os.path.exists(meta_file_path):
+        with open(meta_file_path, "w") as outfile:
+            json.dump({ticker: meta_data}, outfile)
+    else:
+        with open(meta_file_path, "r+") as outfile:
+            data = json.load(outfile)
+            data[ticker] = meta_data
+            outfile.seek(0)
+            json.dump(data, outfile)
+            outfile.truncate()
+
+def exchange_name_check(ticker):
+    with open(f'{data_dir}/meta_data.json', 'rb') as f:
+        meta_data = json.load(f)
+        exchange_name = meta_data[ticker]['exchange']
+    return exchange_name
+
+def file_exists_in_folder(path, filename):
+    for root, dirs, files in os.walk(path):
+        if filename in files:
+            return True
+    return False
+
+
 data_dir = "data_scraping/twelve_data/data"
 years_to_scrape = 13
 tickers = get_tickers()
 
 for ticker in tickers:
     get_api_data(ticker, days = 365*years_to_scrape)
-    break
+    
     
 
